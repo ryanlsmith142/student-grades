cec1d90576bf0c53e6ab0ff599dea575bce58da3
Mitchell Arnold <mitchell.arnold2019@gmail.com>
2019-09-30 12:27:23 -0500

    feat: add student grades revision

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -12,12 +12,40 @@ public class Student {
 
     //STUDENT CONSTRUCTOR
 
-    public Student(String name, ArrayList studentGrades) {
+    public Student(String name) {
         this.name = name;
         this.studentGrades =  new ArrayList<>();
     } //Student()
 
+    public void addGrade(double Grade){
+        studentGrades.add(Grade);
+    }
 
+    public void setName(String sName){
+        this.name = sName;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double[] studentGrades(){
+        return this.studentGrades;
+    }
+
+
+    public static void main(String[] args) {
+        Student ryan = new Student("Ryan Smith");
+        ryan.addGrade(92.12);
+        ryan.addGrade(87.34);
+        ryan.addGrade(84.65);
+
+        for(Student student: ryan.){
+
+        }
+
+        System.out.println("ryan = " + ryan);
+    }
 
 
 } //Student Class
