7aaf3c71df8885a66801400401993e444f307977
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-01 15:58:06 -0500

    start on user display

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,24 +1,43 @@
 package grades;
 
+import util.Input;
+
 import java.util.HashMap;
 
 public class GradesApplication {
 
     public static void main(String[] args) {
-                HashMap<String, Student> students = new HashMap<>();
+        HashMap<String, Student> students = new HashMap<>();
 
-                students.put("ryanlsmith142", new Student("Ryan Smith"));
+        Input userInput = new Input();
 
-                students.put("mitchellarnold", new Student("Mitchell Arnold"));
+        students.put("ryanlsmith142", new Student("Ryan Smith"));
 
-                students.put("mark3000teamplayer", new Student("Mark LeStrange"));
+        students.put("mitchellarnold", new Student("Mitchell Arnold"));
 
-                students.put("izzy93codeguy", new Student("Izzy Bradshires"));
+        students.put("mark3000teamplayer", new Student("Mark LeStrange"));
 
-                System.out.println(students);
+        students.put("izzy93codeguy", new Student("Izzy Bradshires"));
 
-                for(String key : students.keySet()) {
-                    System.out.println(students.containsKey("Izzy Bradshires"));
-                }
+        System.out.println(students);
+
+
+        System.out.println("Welcome!\n");
+        System.out.println("Here are the GitHub usernames of our students: \n");
+
+        for(String studentNames : students.keySet() ) {
+            System.out.println(students.get(studentNames).getName());
+        }
+
+        System.out.println("Please enter a username to see their name and grade average");
+
+        String usernameSelection = userInput.getString();
+
+        for(String studentNames : students.keySet()) {
+            if(usernameSelection.equalsIgnoreCase(students.get(studentNames).getName())) {
+                System.out.println(students.get(studentNames).getGradeAverage(studentNames))
+            }
+        }
+
     }
 }
